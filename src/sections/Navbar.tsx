fdab762e2d460291068d0257bd03786074e6b426
DanBoringName <danjameselliott@outlook.com>
2025-05-29 12:22:42 +0000

    Added logo

--- src/sections/Navbar.tsx
+++ src/sections/Navbar.tsx
@@ -27,7 +27,7 @@ const Navbar = () => {
       <div className="max-w-7xl mx-auto">
         <div className="flex justify-between items-center py-5 mx-auto c-space">
           <a href="/" className="text-neutral-400 font-bold text-xl hover:text-white transition-colors">
-            Bob
+            <img src="/assets/dj-elliott_website_logo.png" alt="DJ-Elliott Logo" className="inline-block h-10 mr-2" />
           </a>
           <button
             onClick={toggleMenu}
