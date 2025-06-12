4884cf648d22a3960f712859ce5030c9ffd71967
DanBoringName <danjameselliott@outlook.com>
2025-06-12 11:45:06 +0000

    Added a dev log section into the discord bot page that discusses the capabilities of the bot as of june

--- src/routes/DiscordBot.tsx
+++ src/routes/DiscordBot.tsx
@@ -1,4 +1,5 @@
 import { navLinks, type NavLink } from "../constants";
+import DevLogs from "../sections/DevLogs";
 import Navbar from "../sections/Navbar";
 
 const Discordbot = () => {
@@ -14,13 +15,12 @@ const Discordbot = () => {
   return (
     <>
       <Navbar navLinks={navLinks} />
-      <section className="min-h-screen w-full flex flex-col relative">
-        <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
-          <p className="sm:text-3xl text-xl font-medium text-white text-center font-sans">
-            Welcome to the discord bot page <span className="waving-hand">🤖</span>
-          </p>
-        </div>
-      </section>
+      <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
+        <p className="sm:text-3xl text-xl font-medium text-white text-center font-sans">
+          Welcome to the discord bot page <span className="waving-hand">🤖</span>
+        </p>
+      </div>
+      <DevLogs />
     </>
   );
 };
---
+++ src/sections/DevLogs.tsx
@@ -0,0 +1,33 @@
+const DevLogs = () => {
+  return (
+    <section className="min-h-screen w-full flex flex-col relative">
+      <p className="sm:text-2xl text-l font-medium text-white font-sans p-3">
+        Dev Logs<span>📝</span>
+      </p>
+      <div className="grid-cols-1 gap-5 h-full">
+        <div className="col-span-1 xl:row-span-3 ">
+          <div className="grid-container">
+            <div>
+              <p className="grid-headtext">2025 - Changes</p>
+              <p className="grid-subtext p-2">
+                The Discord bot has been created. Linked to discord. Linked to google gemini and chatgpt via an api key.
+                A user can now give the /gemini command in the discord guild to query gemini or gpt based on a
+                selection, followed by their query. The AI will respond in a clear and concise manor. NOTE: The name of
+                the command is currently "gemini" which is misleading and will be correctly once a better more general
+                command word is decided.
+              </p>
+              <p className="grid-subtext p-2">
+                A few friends who have wanted to learn a bit about coding have joined as collaborators on the project.
+              </p>
+              <video autoPlay loop muted>
+                <source src="/assets/bot_demo_june.mp4" type="video/mp4" />
+              </video>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default DevLogs;
